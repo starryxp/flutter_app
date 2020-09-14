01b202607954ef813cdf1fa789ae62f213a73f32
xiaopeng <XIAOPENG@ceair.com>
2020-09-14 16:01:48 +0800

    Icon

--- lib/widgets/basic/IconPage.dart
+++ lib/widgets/basic/IconPage.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/utils/ToastUtil.dart';
 
 class IconPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Icon'),
+        title: Text('Icon：A Material Design icon.'),
       ),
       body: ListView(
-        children: [],
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.favorite,
+                color: Colors.blue,
+                size: 24,
+              ),
+              Icon(
+                Icons.audiotrack,
+                color: Colors.red,
+                size: 24,
+              ),
+              Icon(
+                Icons.people,
+                color: Colors.yellow,
+                size: 24,
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                  size: 24,
+                ),
+                onPressed: () {
+                  ToastUtil.showToast('onPressed');
+                },
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
